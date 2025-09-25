169827a9887d8d1322ba3612ff7e7f83582999bb
gabrieljorda <gabrieleminhamae@hotmail.com>
2025-09-25 20:37:28 -0300

    .

--- aula4/react-aula4/src/ItemPage.js
+++ aula4/react-aula4/src/ItemPage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./ItemPage.css";
+import Item from "./Item";
 
 function ItemPage({ items }) {
   return (
     <ul className="ItemPage-items">
       {items.map((item) => (
         <li key={item.id} className="ItemPage-item">
-          {item.name}
+          <Item item={item } onAddToCart={()=> onAddToCart(item)}></Item>}
         </li>
       ))}
     </ul>
