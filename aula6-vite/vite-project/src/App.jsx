92c1d2ca9451367189f2fcc0b79d5d30bf976da7
gabrieljorda <gabrieleminhamae@hotmail.com>
2025-09-30 20:33:21 -0300

    .

---
+++ aula6-vite/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import ConditionalComponent from "./component/ConditionalComponent";
+
+function App() {
+  return (
+      <div>
+        <ConditionalComponent/>
+      </div>
+  );
+}
+
+export default App;
